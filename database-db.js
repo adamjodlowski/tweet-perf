a3a1d5d40d50df434911601831570e81846e3515
Daniel Mendalka <daniel.mendalka@gmail.com>
2011-09-03 15:20:59 +0200

    working timeline in database-db

--- database-db.js
+++ database-db.js
@@ -23,43 +23,36 @@ for (i = 1; i < 5; i++) {
     }(i));
 }
 
-var USERS = 'users';
-var STATUSES = 'statuses';
-var FOLLOWERS = 'followers';
 var LIMIT = 20;
 
 function Database() {
     
 }
 
-Database.prototype.findUser = function (username, callback) {
-    var i;
-    
-    callback([
-        {"created_at": "Fri Jul 16 16:58:46 +0000 2010",
-        "text": "got a lovely surprise from @craftybeans.",
-        "id": 18700887835},
-        {"created_at": "Fri Jul 16 16:55:52 +0000 2010",
-        "text": "Anything is possible when you're in",
-        "id": 18700688341}
-    ]);
-}
-
-Database.prototype.query = function(createQuery, callback) {
+Database.prototype.query = function(createQuery, callback, queryAll) {
     var queriesDone = 0;
     var results = [];
+    var logResultsCount = [];
+    
     for (i = 1; i < clients.length; i++) {
         (function(client, i){
             createQuery(client.query(), i).execute(function(error, rows, cols){
                 if (error) {
                     console.log('SQL ERROR: ' + error);
                 } else {
-                    results += rows;
+                    results = results.concat(rows);
+                    logResultsCount[i] = rows.length;
+                    
                     queriesDone++;
-                    if (rows.length) {
-                        callback(rows);
-                    }
-                    if (queriesDone === (clients.length - 1)) {
+                    
+                    if (queryAll === false && rows.length) {    
+                        if(cols.length === 1 && rows.length === 1) {
+                            callback(rows[0][cols[0].name]);
+                        } else {
+                            callback(rows);
+                        }
+                    } else if (queryAll !== false && queriesDone === (clients.length - 1)) {
+                        console.log('Results count: '+ logResultsCount);
                         callback(results);
                     }
                 }
@@ -68,11 +61,36 @@ Database.prototype.query = function(createQuery, callback) {
     }
 }
 
+Database.prototype.findUserId = function (username, callback) {
+    this.query(function(query) {
+        return query
+            .select('users.id')
+            .from('users')
+            .where('users.screen_name = ?', [username])
+            .limit(1);
+    }, callback, false);
+}
+
+Database.prototype.selectFollowed = function (username, callback) {
+    var that = this;
+    that.findUserId(username, function(user_id){
+        that.query(function(query){
+            return query
+                .select('user_id')
+                .from('followers')
+                .where('followers.follower_id = ?', [user_id]);
+        }, function(results) {
+            results.push(user_id);
+            callback(results);
+        });
+    });
+}
+
 Database.prototype.selectTweets = function (username, callback) {
-    this.query(function(query, client_index) {
+    this.query(function(query, client_index){
         return query
             .select('users.screen_name, statuses.text, statuses.created_at')
-            .from(USERS)
+            .from('users')
             .join({type: 'LEFT', table: 'statuses', conditions: 'users.id = statuses.user_id', escape: false})
             .where('users.screen_name = ?', [username])
             .order('statuses.created_at')
@@ -85,7 +103,23 @@ Database.prototype.insertTweet = function (username, status, callback) {
 };
 
 Database.prototype.selectTimeline = function (username, callback) {
-    
+    var that = this;
+    that.selectFollowed(username, function(results){
+        for (var i in results) {
+            if (typeof results[i] === 'object') {
+                results[i] = results[i].user_id;
+            }
+        }
+        that.query(function(query){
+            return query
+                .select('users.screen_name, statuses.text, statuses.created_at')
+                .from('users')
+                .join({type: 'LEFT', table: 'statuses', conditions: 'users.id = statuses.user_id', escape: false})
+                .where('users.id IN (?)', [results.join(',')])
+                .order('statuses.created_at')
+                .limit(LIMIT);
+        }, callback);
+    });
 }
 
 exports.Database = Database;
